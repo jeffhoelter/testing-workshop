371d654baaddc29024e378a9ffcb147c1484a760
jeffhoelter <jeffh@loar.net>
2017-07-28 12:58:27 -0400

    Completed first exercise.

--- api/demo/unit/__tests__/make-me-a-sandwich.js
+++ api/demo/unit/__tests__/make-me-a-sandwich.js
@@ -1,1 +1,20 @@
-test('todo', () => {})
+import makeMeASandwich from '../make-me-a-sandwich'
+
+test('returns null if the sandwich does not exist', () => {
+  const req = createRequestObject()
+  const result = makeMeASandwich(req)
+
+  expect(result).toBeNull()
+})
+
+test('returns my sandwich', () => {
+  const sandwich = 'Monte Cristo'
+  const req = createRequestObject(sandwich)
+  const result = makeMeASandwich(req)
+
+  expect(result).toBe(sandwich)
+})
+
+function createRequestObject(sandwich) {
+  return {query: {sandwich}}
+}
